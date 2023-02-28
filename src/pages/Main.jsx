6752ed24c7f796614a8b55a7fb4a2c82aa25d88a
MA <mehmedak@gmail.com>
2023-02-28 13:51:37 +0300

    added the data

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,7 @@ import FilmYear from '../components/FilmYear';
 import FilmInfo from '../components/FilmInfo';
 
 const Main = () => {
-// console.log(data);
+  // console.log(data);
   return (
     <div className="d-block d-sm-block d-lg-flex justify-content-evenly align-items-center my-div"> 
       <FilmYear data={data} />
